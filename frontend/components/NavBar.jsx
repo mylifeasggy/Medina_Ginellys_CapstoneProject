9ca4bb80f0f03184965705bec4126c0f3ecad9be
mylifeasggy <ginellysmsuarez@gmail.com>
2025-07-15 14:59:16 -0400

    Working on header and navbar

---
+++ frontend/components/NavBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const NavBar = () => {
+    return (
+        <nav className='Main-nav'>
+                <a> Home </a>
+                <a> Recipes </a>
+                <a> Tips </a>
+                <a> Form </a>
+        
+        </nav>
+    );
+}
+
+export default NavBar;
