97b6a59d53643d76e11d207ee1c83debb421ba5a
mylifeasggy <ginellysmsuarez@gmail.com>
2025-07-16 13:02:15 -0400

    trying to do  the connection between front end backend

--- frontend/components/SearchBar.jsx
+++ frontend/components/SearchBar.jsx
@@ -1,33 +1,49 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 
 const SearchBar = () => {
-    
-    const [ingrediente, setIngredient]= useState([])
+
+    const [ingredient, setIngredient] = useState([])
+    const [recipe, setRecipe] = useState("")
+
+    const inputRef = useRef()
+
+    function enterIngredient(formData) {
+
+        const newIngredient = formData.get("ingredient")
+        if (newIngredient.trim() === "") return;
+
+        setIngredient((prev) => [...prev, newIngredient])
+
+    }
 
 
+    async function submitIngredient(e) {
+        e.preventDefault();
+        const formData = {
+            text: inputRef.current.value
+        }
+        const response = await fetch('http://localhost:3000/', {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(formData),
+        })
 
+        await response.json()
 
-
-
-
-
-
-
-
-
+    }
 
 
     return (
         <div className="main-container">
-            <form className="search-ingredient">
-                <input type="text"
-                id="search-bar"
-                name="ingredient"
-                placeholder="e.g cheese"
-                /> 
+            <form className="search-ingredient" onSubmit={submitIngredient}>
+                <input type="text" ref={inputRef}
+                    id="search-bar"
+                    name="ingredient"
+                    placeholder="e.g cheese"
+                />
                 <button type="submit" className="add">ADD INGREDIENT</button>
             </form>
-            
+
         </div>
     );
 }
