210a4f3ec1554c77dfb249893482be7c679126a2
mylifeasggy <ginellysmsuarez@gmail.com>
2025-07-16 13:01:41 -0400

    API set up

---
+++ backend/api/apiAI.js
@@ -0,0 +1,30 @@
+import Anthropic from "@anthropic-ai/sdk"
+
+const api_key=process.env.API_KEY
+
+
+
+const SYSTEM_PROMPT = `
+You are an assistant that receives a list of ingredients that a user has and suggests a recipe they could make with some or all of those ingredients. You don't need to use every ingredient they mention in your recipe. The recipe can include additional ingredients they didn't mention, but try not to include too many extra ingredients. Format your response in markdown to make it easier to render to a web page`
+
+
+const anthropic = new Anthropic({
+    apiKey: api_key,
+});
+
+export async function getRecipeFromAPI(ingredientsArr) {
+
+    const ingredientsString = ingredientsArr.join(", ")
+
+    const msg = await anthropic.messages.create({
+        model: "claude-3-haiku-20240307",
+        max_tokens: 1024,
+        temperature:1,
+        system: SYSTEM_PROMPT,
+        messages: [
+            { role: "user", content: `I have ${ingredientsString}. Please give me a recipe you'd recommend I make!` },
+        ],
+    });
+    return msg.content[0].text
+}
+
