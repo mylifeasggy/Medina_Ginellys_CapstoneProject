711ad4154320b40392f9d17d5d16552f03411780
mylifeasggy <ginellysmsuarez@gmail.com>
2025-07-16 10:19:37 -0400

    searchbar styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import Header from "../components/Header"
+import SearchBar from "../components/SearchBar"
 
 function App() {
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
     <Header/>
+    <SearchBar/>
     </>
   )
 }
