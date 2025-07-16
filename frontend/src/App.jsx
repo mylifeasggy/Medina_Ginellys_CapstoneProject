d5cbf2e01b002585446a56af9ecc9c66e4715ae3
mylifeasggy <ginellysmsuarez@gmail.com>
2025-07-16 14:36:23 -0400

    delete some code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,15 @@
+import { useEffect, useState } from "react"
 import Header from "../components/Header"
 import SearchBar from "../components/SearchBar"
 
 function App() {
 
 
+
   return (
     <>
-    <Header/>
-    <SearchBar/>
+      <Header />
+      <SearchBar />
     </>
   )
 }
