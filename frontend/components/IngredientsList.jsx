c1a9843c17da93169f7924eb13ea6ddbda6a6bf5
mylifeasggy <ginellysmsuarez@gmail.com>
2025-07-16 14:35:58 -0400

    need styling

--- frontend/components/IngredientsList.jsx
+++ frontend/components/IngredientsList.jsx
@@ -8,7 +8,21 @@ function IngredientsList({ ingredients, getRecipe, deleteIngredient }) {
         </li>
     ));
 
+    return (
+    <section>
 
+      <ul className="ingredient-list"> {ing} </ul>
+      {ingredients.length >= 3 && <div className="get-recipe">
+        <div>
+          <h3> Generate a recipe from your list of ingredients</h3>
+        </div>
+        <button onClick={getRecipe}> Get a recipe </button>
 
-
+      </div>}
+    </section>
+  );
 }
+
+export default IngredientsList;
+
+
