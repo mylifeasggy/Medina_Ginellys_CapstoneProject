417565969eedcb515f5aec8170e473d737d01535
mylifeasggy <ginellysmsuarez@gmail.com>
2025-07-17 13:37:01 -0400

    recipe item for display

---
+++ frontend/pages/RecipeItem.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const RecipeItem = () => {
+
+
+
+
+
+    return (
+        <>
+            <div className='item-container'>
+                <img src=''/>
+
+            </div>
+        </>
+    );
+}
+
+export default RecipeItem;
