93b0f07777c85ea4ad78b9a523694ec43eae87a7
mylifeasggy <ginellysmsuarez@gmail.com>
2025-07-17 19:13:44 -0400

    not working when I put a recipe doesnt exist

--- frontend/pages/Recipes.jsx
+++ frontend/pages/Recipes.jsx
@@ -6,7 +6,11 @@ import { FaSearch } from "react-icons/fa";
 
 
 const Recipes = () => {
-    const [item, setItem]=useState([]);
+
+    const [item, setItem] = useState([]);
+    const [isRecipe, setisRecipe]= useState(true)
+    const inputRef = useRef()
+
     const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=a`
 
     useEffect(() => {
@@ -29,22 +33,43 @@ const Recipes = () => {
 
     }, [url])
 
+    async function handleRecipe(e) {
+        e.preventDefault();
+        let recipe = inputRef.current.value.trim();
+        if (!recipe) return;
+
+        try {
+            const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${recipe}`)
+            const data = await response.json();
+            setItem(data.meals)
+            recipe = "";
+        } catch (e) {
+            console.log(e);
+            setisRecipe(false)
+        }
+        recipe = inputRef.current.value = "";
+    }
+
+
+
+
+
 
 
     return (
         <div className="main-container">
-            <form className="search-ingredient" >
-                <h1> Search a Recipe </h1>
+            <form className="search-ingredient" onSubmit={handleRecipe}>
                 <input type="text"
+                    ref={inputRef}
                     id="search-bar"
-                    placeholder="e.g Pasta"
+                    placeholder="Search a recipe"
                 />
                 <button type="submit" className="add"><FaSearch /></button>
             </form>
             <div>
-                <RecipeItem
-                data={item}
-                 />
+                {isRecipe ? (<RecipeItem
+                    data={item} />) : ( <p style={{textAlign: 'center'}}>'No recipes found'</p>
+                )}
 
             </div>
 
