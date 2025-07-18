13193a31ea11ea4cd65c71012ba5549391748f05
mylifeasggy <ginellysmsuarez@gmail.com>
2025-07-17 20:45:27 -0400

    Not idea how connect the schema and frontform

--- frontend/pages/Form/Form.jsx
+++ frontend/pages/Form/Form.jsx
@@ -3,28 +3,76 @@ import React from 'react';
 
 
 const url= import.meta.env.VITE_BASE_URL
+  const inputRef = useRef()
 
 const Form = () => {
     
     const [form, setForm] = useState([]);
 
-    async function getForm() {
+    /*async function getForm() {
         const response = await fetch(`${url}/form`)
+        const data =
         
         
     }
+*/
 
-
+async function handleSubmite(e) {
+    
+}
 
 
 
 
 
     return (
-        <div>
+        <div className='form-container'>
+            <form>
+                <input type="text" name='recipe_name' required/>
+                <input type='text' name="author" required/>
+                <input type="text" name="ingredients" required/>
+                <input type='number' name='cook_time' required/>
+                <input type='number' name='servings'/>
+                <input type="text" name="directions" required/>
+                <input type='text' name="notes"/>
+                <button>Submit Recipe</button>
+            </form>
             
         </div>
     );
 }
+
+
+
+/*  recipe_name: {
+        type: String,
+        required:true,
+    },
+
+    author: {
+        type:String,
+        required:true,
+    },
+    ingredients: {
+        type:[String],
+        required:true
+    },
+    cook_time: {
+        type:Number,
+        required:true
+    },
+    servings: {
+        type:Number
+    },
+    directions: {
+        type:String,
+        required:true
+    },
+    notes:{
+        type:String
+
+    }
+ */
+
 
 export default Form;
